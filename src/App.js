da4306379db1f916f0cec28a4c0de314aa7d3d0a
Sushannt <kumar.su5hant@yahoo.com>
2024-03-02 18:39:50 +0530

    [fix]: "minor change"

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 
-import Dashboard from "./pages/Dashbaord";
 import { Routes, Route } from "react-router-dom";
+
 //pages
+import Dashboard from "./pages/Dashbaord";
 import Login from "./pages/Login";
 import Error404 from "./pages/Error404";
+
 const App = () => {
   return (
     <div>
