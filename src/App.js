59782eb5ee9ab6674703079c5e5b08638f6017ed
Sushannt <kumar.su5hant@yahoo.com>
2024-03-02 18:38:08 +0530

    [fix]: "routes set properly"

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Dashboard from './pages/Dashbaord';
-import { Routes, Route } from 'react-router-dom';
+import Dashboard from "./pages/Dashbaord";
+import { Routes, Route } from "react-router-dom";
 //pages
-import Login from './pages/Login';
-import Error404 from './pages/Error404';
+import Login from "./pages/Login";
+import Error404 from "./pages/Error404";
 const App = () => {
-  
   return (
-    
     <div>
       <Routes>
-      <Route path="/dashboard" element={<Dashboard/>}/>
-       <Route path="/" element={<Login />}>
-       
-       <Route path="*" element={<Error404 />} />
-     </Route>
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
