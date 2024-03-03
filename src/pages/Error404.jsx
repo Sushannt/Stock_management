759f01428267725f9df6b754d6145e6a68e58dcb
Sushannt <kumar.su5hant@yahoo.com>
2024-03-03 14:50:09 +0530

    [update]: "added 404 page"

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,6 +1,30 @@
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 const Error404 = () => {
-    return <div>404 - NOT FOUND!</div>;
-  };
-  
-  export default Error404;
-  
+  return (
+    <div
+      className="d-flex align-items-center bg-tertiary"
+      style={{ height: "100vh" }}
+    >
+      <Container fluid>
+        <Row className="text-center h-100">
+          <Col lg={12}>
+            <h1 style={{ fontSize: "15rem" }}>404 </h1>
+          </Col>
+          <Col lg={12}>
+            <h1 className="display-3">Page Not Found</h1>
+          </Col>
+          <Col lg={12}>
+            <Link to={"/dashboard"}>
+              <Button variant="secondary" size="lg">
+                Back to Dashboard
+              </Button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+};
+
+export default Error404;
