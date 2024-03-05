33891a661247e209d805ce7d1d7b5f5f2d5ba60a
amit-Kumar07 <144255599+amit-Kumar07@users.noreply.github.com>
2024-03-05 10:09:02 +0530

    add Roles in masters

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { DASHBOARD_SIDEBAR_LINKS, DASHBOARD_SIDEBAR_LINKS2 } from "../lib/consts/Navigation";
 import { useState } from 'react';
+import {Link} from 'react-router-dom';
 function Sidebar() {
     const [activeTab, setActiveTab] = useState(0);
   const [activeNavIndex1, setActiveNavIndex1] = useState(0);
@@ -20,7 +21,7 @@ function Sidebar() {
     <div className="nav-left-sidebar sidebar-dark">
         <div className="menu-list">
             <nav className="navbar navbar-expand-lg navbar-light">
-                <a className="d-xl-none d-lg-none" href="#">Dashboard</a>
+                <Link to='#' className="d-xl-none d-lg-none">Dashboard</Link>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -30,7 +31,7 @@ function Sidebar() {
                             Menu
                         </li>
                         <li className="nav-item ">
-                            <a className="nav-link active" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i className="fa fa-fw fa-user-circle"></i>Masters <span className="badge badge-success">6</span></a>
+                            <Link className="nav-link active" to="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-1" aria-controls="submenu-1"><i className="fa fa-fw fa-user-circle"></i>Masters <span className="badge badge-success">6</span></Link>
                             <div id="submenu-1" className="collapse submenu">
                                 <div className="nav flex-column">
                                     {DASHBOARD_SIDEBAR_LINKS.map((item, index) => (
@@ -45,7 +46,7 @@ function Sidebar() {
                             </div>
                         </li>
                         <li className="nav-item">
-                            <a className="nav-link" href="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i className="fa fa-fw fa-rocket"></i>Input Form</a>
+                            <Link className="nav-link" to="#" data-toggle="collapse" aria-expanded="false" data-target="#submenu-2" aria-controls="submenu-2"><i className="fa fa-fw fa-rocket"></i>Input Form</Link>
                             <div id="submenu-2" className="collapse submenu">
                                 <div className="nav flex-column">
                                 {DASHBOARD_SIDEBAR_LINKS2.map((item, index) => (
