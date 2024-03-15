303cc32990c4cce4af6b4af380c6708abeee8e16
Sushannt <kumar.su5hant@yahoo.com>
2024-03-15 09:44:33 +0530

    [update]: "added department endpoints"

--- src/constants.jsx
+++ src/constants.jsx
@@ -1,7 +1,13 @@
 export const LOGIN_URL = "/api/Account/Login";
+
+// ROLE ENDPOINT
 export const ADD_ROLE = "/api/Account/AddRole";
 export const GET_ROLES = "/api/Account/GetRoles";
 export const UPDATE_ROLE = "/api/Account/UpdateRole";
 export const DELETE_ROLE = "/api/Account/DeleteRole";
-export const ADD_DEPARTMENT = "/api/DepartmentMaster/AddDepartment";
-export const GET_DEPARTMENT = '/api/DepartmentMaster/GetDeparment'
+
+//DEPARTMENT ENDPOINT
+export const GET_DEPARTMENT = "/api/DepartmentMaster/Get";
+export const CREATE_DEPARTMENT = "/api/DepartmentMaster/create";
+export const EDIT_DEPARTMENT = "/api/DepartmentMaster/edit";
+export const DELETE_DEPARTMENT = "/api/DepartmentMaster/delete";
